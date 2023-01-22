44dab4d415434202f80c05801b8c40bd343455f5
Sergei Trofimovich <slyich@gmail.com>
2023-01-22 12:02:51 +0000

    src/main.rs: move repology handling to src/repology.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use crate::error::*;
 mod cmd;
 use crate::cmd::*;
 
+mod repology;
+
 /// Installed packages with available 'pname' and 'version' attributes.
 #[derive(Eq, PartialEq, Ord, PartialOrd)]
 struct LocalInstalledPackage {
@@ -244,92 +246,6 @@ fn get_local_available_packages(nixpkgs: &Option<String>)
     Ok(r)
 }
 
-/// Installed packages with available 'pname' and 'version' attributes.
-#[derive(Eq, PartialEq, Ord, PartialOrd)]
-struct RepologyPackage {
-    /// repology package name
-    repology_name: String,
-
-    /// nixpkgs 'pname' from available packages
-    name: String,
-
-    version: String,
-    /// repology's characterization of the state: outdated, dev-only, etc.
-    status: String,
-
-    /// latest version available in some other repository
-    /// Might not exist if latest version was added and then
-    /// removed from repology.org.
-    latest: Option<String>,
-}
-
-/// Returns list of all outdated derivations according to repology.
-fn get_repology_packages(verbose: bool, cancel_fetch: &AtomicBool)
-    -> Result<BTreeSet<RepologyPackage>, OldeError> {
-    let mut r = BTreeSet::new();
-
-    // We pull in all package ingo py paginating through
-    //     https://repology.org/api/v1/projects/?inrepo=nix_unstable&outdated=1
-    //     https://repology.org/api/v1/projects/${suffix}?inrepo=nix_unstable&outdated=1
-    let mut suffix: String = "".to_string();
-
-    loop {
-        if cancel_fetch.load(Ordering::Relaxed) {
-            return Err(OldeError::Canceled(String::from("Repology fetch")));
-        }
-        let url = format!("https://repology.org/api/v1/projects/{suffix}?inrepo=nix_unstable&outdated=1");
-
-        if verbose {
-            eprintln!("Fetching from repology: {:?}", suffix);
-        }
-        let contents_u8 = run_cmd(&["curl", "--compressed", "-s", &url])?;
-        // {
-        //   "python:networkx": [
-        //     {
-        //       "repo": "nix_unstable",
-        //       "name": "python3.10-networkx",
-        //       "version": "2.8.6",
-        //       "status": "outdated",
-        //     },
-
-        #[derive(Deserialize, Debug)]
-        /// Dervivation description with subset of fields needed to detect outdated packages.
-        struct Repology { repo: String, name: Option<String>, version: String, status: String }
-
-        let pkgs: BTreeMap<String, Vec<Repology>> =
-            serde_json::from_slice(contents_u8.as_slice()).expect("valid json");
-
-        let mut next_suffix = suffix.clone();
-        for (n, vs) in &pkgs {
-            next_suffix = n.clone() + "/";
-
-            let olatest_entry =
-                vs.iter()
-                  .find_map(|e| if e.status == "newest" || e.status == "unique" { Some(e) } else { None });
-            let latest = olatest_entry.map(|e| e.version.clone());
-
-            // There can be multiple nix_unstable package entries for a
-            // single repology entry: pycropto vs pycryptodome.
-            // Store all of them.
-            for v in vs {
-                if v.repo != "nix_unstable" { continue }
-
-                r.insert(RepologyPackage {
-                    repology_name: n.clone(),
-                    name: v.name.clone().expect("name"),
-                    version: v.version.clone(),
-                    status: v.status.clone(),
-                    latest: latest.clone(),
-                });
-            }
-        }
-        if suffix == next_suffix { break }
-        suffix = next_suffix;
-    }
-
-    Ok(r)
-}
-
 /// A tool to show outdated packages in current system according to
 /// repology.org database.
 #[derive(Parser, Debug)]
@@ -349,7 +265,7 @@ fn main() -> Result<(), OldeError> {
     let o = Args::parse();
 
     let (r, i, a) = (|| {
-       let mut r: Result<BTreeSet::<RepologyPackage>,       OldeError> =
+       let mut r: Result<BTreeSet::<repology::Package>,       OldeError> =
            Ok(BTreeSet::new());
        let mut i: Result<BTreeSet::<LocalInstalledPackage>, OldeError> =
            Ok(BTreeSet::new());
@@ -366,7 +282,7 @@ fn main() -> Result<(), OldeError> {
        std::thread::scope(|s| {
          s.spawn(|| {
              eprintln!("Fetching 'repology' ...");
-             r = get_repology_packages(o.verbose, &cancel_fetch);
+             r = repology::get_packages(o.verbose, &cancel_fetch);
              if r.is_err() {
                  cancel_fetch.store(true, Ordering::Relaxed);
                  eprintln!("... 'repology' failed.");
---
+++ src/repology.rs
@@ -0,0 +1,95 @@
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+
+use serde_derive::Deserialize;
+use std::sync::atomic::{AtomicBool,Ordering};
+
+use crate::error::*;
+use crate::cmd::*;
+
+/// Installed packages with available 'pname' and 'version' attributes.
+#[derive(Eq, PartialEq, Ord, PartialOrd)]
+pub(crate) struct Package {
+    /// repology package name
+    pub(crate) repology_name: String,
+
+    /// nixpkgs 'pname' from available packages
+    pub(crate) name: String,
+
+    version: String,
+    /// repology's characterization of the state: outdated, dev-only, etc.
+    status: String,
+
+    /// latest version available in some other repository
+    /// Might not exist if latest version was added and then
+    /// removed from repology.org.
+    pub(crate) latest: Option<String>,
+}
+
+/// Returns list of all outdated derivations according to repology.
+pub(crate) fn get_packages(verbose: bool, cancel_fetch: &AtomicBool)
+    -> Result<BTreeSet<Package>, OldeError> {
+    let mut r = BTreeSet::new();
+
+    // We pull in all package ingo py paginating through
+    //     https://repology.org/api/v1/projects/?inrepo=nix_unstable&outdated=1
+    //     https://repology.org/api/v1/projects/${suffix}?inrepo=nix_unstable&outdated=1
+    let mut suffix: String = "".to_string();
+
+    loop {
+        if cancel_fetch.load(Ordering::Relaxed) {
+            return Err(OldeError::Canceled(String::from("Repology fetch")));
+        }
+        let url = format!("https://repology.org/api/v1/projects/{suffix}?inrepo=nix_unstable&outdated=1");
+
+        if verbose {
+            eprintln!("Fetching from repology: {:?}", suffix);
+        }
+        let contents_u8 = run_cmd(&["curl", "--compressed", "-s", &url])?;
+        // {
+        //   "python:networkx": [
+        //     {
+        //       "repo": "nix_unstable",
+        //       "name": "python3.10-networkx",
+        //       "version": "2.8.6",
+        //       "status": "outdated",
+        //     },
+
+        #[derive(Deserialize, Debug)]
+        /// Dervivation description with subset of fields needed to detect outdated packages.
+        struct Repology { repo: String, name: Option<String>, version: String, status: String }
+
+        let pkgs: BTreeMap<String, Vec<Repology>> =
+            serde_json::from_slice(contents_u8.as_slice()).expect("valid json");
+
+        let mut next_suffix = suffix.clone();
+        for (n, vs) in &pkgs {
+            next_suffix = n.clone() + "/";
+
+            let olatest_entry =
+                vs.iter()
+                  .find_map(|e| if e.status == "newest" || e.status == "unique" { Some(e) } else { None });
+            let latest = olatest_entry.map(|e| e.version.clone());
+
+            // There can be multiple nix_unstable package entries for a
+            // single repology entry: pycropto vs pycryptodome.
+            // Store all of them.
+            for v in vs {
+                if v.repo != "nix_unstable" { continue }
+
+                r.insert(Package {
+                    repology_name: n.clone(),
+                    name: v.name.clone().expect("name"),
+                    version: v.version.clone(),
+                    status: v.status.clone(),
+                    latest: latest.clone(),
+                });
+            }
+        }
+        if suffix == next_suffix { break }
+        suffix = next_suffix;
+    }
+
+    Ok(r)
+}
+
