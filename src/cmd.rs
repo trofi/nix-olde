9ea7a525e06a0feb865bd0dc998a172b0f17714b
Sergei Trofimovich <slyich@gmail.com>
2023-08-09 20:51:15 +0100

    src/cmd.rs: always print failed command and it's stderr on info+ level
    With the change command failures are a bit more comprehensible:
    
        $ nix run .# -- -f /etc/nixos#does-not-exist
        warning: Git tree '/home/slyfox/dev/nix-olde' is dirty
        Fetching 'repology'
        Fetching 'installed'
        Fetching 'available'
        INFO: Failed running ["nix", "--extra-experimental-features", "nix-command", "--extra-experimental-features", "flakes", "eval", "--impure", "--raw", "/etc/nixos#nixosConfigurations.does-not-exist.config.system.build.toplevel.drvPath"]: ExitStatus(unix_wait_status(256))
        INFO: Result of ["nix", "--extra-experimental-features", "nix-command", "--extra-experimental-features", "flakes", "eval", "--impure", "--raw", "/etc/nixos#nixosConfigurations.does-not-exist.config.system.build.toplevel.drvPath"]: Output { status: ExitStatus(unix_wait_status(256)), stdout: "", stderr: "\r\u{1b}[K\r\u{1b}[Kerror: flake 'git+file:///etc/nixos' does not provide attribute 'packages.x86_64-linux.nixosConfigurations.does-not-exist.config.system.build.toplevel.drvPath', 'legacyPackages.x86_64-linux.nixosConfigurations.does-not-exist.config.system.build.toplevel.drvPath' or 'nixosConfigurations.does-not-exist.config.system.build.toplevel.drvPath'\n" }
        'installed' done, took 5.36 s.

--- src/cmd.rs
+++ src/cmd.rs
@@ -5,14 +5,18 @@ use crate::error::*;
 /// Runs 'cmd' and returns stdout or failure.
 pub(crate) fn run_cmd(args: &[&str]) -> Result<Vec<u8>, OldeError> {
     let output = Command::new(args[0]).args(&args[1..]).output()?;
-    log::debug!("Running {:?}: {:?}", args, output.status);
-    log::trace!("Result of {:?}: {:?}", args, output);
 
     if !output.status.success() {
+        // Be verbose about all command run failures.
+        log::info!("Failed running {:?}: {:?}", args, output.status);
+        log::info!("Result of {:?}: {:?}", args, output);
         return Err(OldeError::CommandFailed {
             cmd: args.iter().map(|a| a.to_string()).collect(),
             output,
         });
+    } else {
+        log::debug!("Running {:?}: {:?}", args, output.status);
+        log::trace!("Result of {:?}: {:?}", args, output);
     }
 
     Ok(output.stdout)
