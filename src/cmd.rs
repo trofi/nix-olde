91e8134501bc51bb915ea01eeb4509edc7127b95
Sergei Trofimovich <slyich@gmail.com>
2025-10-20 21:09:34 +0100

    src/cmd.rs: add time metering for individual commands
    THe change adds basic timigs information as:
    
        DEBUG: Ran ["curl", ...]: ExitStatus(unix_wait_status(0)); took=4.27 s
    
    Should help a bit with debugging of https://github.com/trofi/nix-olde/issues/191
    and https://github.com/trofi/nix-olde/issues/192.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,14 +1,22 @@
 use std::process::Command;
+use std::time::Instant;
 
 use crate::error::*;
 
 /// Runs 'cmd' and returns stdout or failure.
 pub(crate) fn run_cmd(args: &[&str]) -> Result<Vec<u8>, OldeError> {
+    let start_time = Instant::now();
     let output = Command::new(args[0]).args(&args[1..]).output()?;
+    let took = start_time.elapsed().as_secs_f64();
 
     if !output.status.success() {
         // Be verbose about all command run failures.
-        log::info!("Failed to run {:?}: {:?}", args, output.status);
+        log::info!(
+            "Failed to run {:?}: {:?}; took={:.2} s",
+            args,
+            output.status,
+            took
+        );
         for l in output
             .stdout
             .split(|c| *c == b'\n')
@@ -28,7 +36,7 @@ pub(crate) fn run_cmd(args: &[&str]) -> Result<Vec<u8>, OldeError> {
             output,
         });
     } else {
-        log::debug!("Ran {:?}: {:?}", args, output.status);
+        log::debug!("Ran {:?}: {:?}; took={:.2} s", args, output.status, took);
         for l in output
             .stdout
             .split(|c| *c == b'\n')
